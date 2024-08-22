ab033fc641259a7754bae71b874b3cf289cdd60a
Dipak Giri <dipakgiri1200@gmail.com>
2024-08-22 21:06:18 +0545

    added experimental flag to nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["via.placeholder.com", "img.clerk.com", "res.cloudinary.com"],
   },
+  experimental: {
+    ppr: "incremental",
+  },
 };
 
 export default nextConfig;
